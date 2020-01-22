a0a6a3e522ea450172e4946e1206d527a9c32718
vivekhegde <vivekhegde85@gmail.com>
2020-01-22 15:06:04 +0530

    3 files modified

--- index.ts
+++ index.ts
@@ -2,5 +2,10 @@
 import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById("app");
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// const appDiv: HTMLElement = document.getElementById("app");
+// appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// const dd5: HTMLElement = document.getElementById("dd5");
+// dd5.addEventListener("click", () => {
+//   dd5.classList.toggle("active");
+//   return false;
+// });
